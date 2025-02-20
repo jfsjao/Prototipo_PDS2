be306857c28ab0b58895695277ef695d4ae2514c
joaofelipes <joao.felipe@alfaebeto.org.br>
2025-02-20 16:04:47 -0300

    primeira ação

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "lala"}
+    return {"Hello": "lalalalalalalalala"}
 
 @app.post("/criar")
 def criar_valores(nova_mensagem: classes.Mensagem):
