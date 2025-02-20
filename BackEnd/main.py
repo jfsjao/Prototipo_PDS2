2738af9016e7145fe6c64bd9e1b101a3b17decdb
joaofelipes <joao.felipe@alfaebeto.org.br>
2025-02-20 14:49:26 -0300

    aula 3 - Postman

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "lala"}
+
+@app.post("/criar")
+def criar_valores(res: dict = Body(...)):
+    print(res)
+    return {"Mensagem": f"lala: {res['lala']} lele: {res['lele']}"}
