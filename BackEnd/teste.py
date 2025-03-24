93e84d17cf85fdbc8336fe75c8e142ceea8c2932
joaofelipes <joao.felipe@alfaebeto.org.br>
2025-03-23 23:15:49 -0300

    terceira ação

--- BackEnd/teste.py
+++ BackEnd/teste.py
@@ -4,11 +4,10 @@ from main import app
 client = TestClient(app)
 
 def teste_hello():
-
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "lala"}
-    
+    assert response.json() == {"message": "API está rodando!"}
+
 def teste_quadrado():
     num = 4
     response = client.get(f"/quadrado/{num}")
